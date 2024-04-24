a9c6c9e223323c0914f8b254ff4ed6cd9f5094a7
valentin <valentinlollagalvan@hotmail.com>
2024-04-24 19:59:01 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+/*var swiper = new Swiper ("mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween:30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickeable:true,
+    },
+    navigation: {
+        nextE1: "swiper-button-next",
+        prevE1:"swiper-button-prev",
+    }
+})
+
+var swiper = new Swiper ("mySwiper-2", {
+    slidesPerView:3,
+    spaceBetween:20,
+    loop:true,
+    loopFillGroupwithBlank:true,
+    navigation: {
+        nextE1: "swiper-button-next",
+        prevE1:"swiper-button-prev",
+    }
+})
+*/
